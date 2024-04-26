b8f3fa46ebda09387f9033c816e0895bdd2bc2f9
kevinmedrano21 <kmedrano433@gmail.com>
2024-04-26 14:34:38 -0600

    Frontend RoouteRover1

--- Index.js
+++ Index.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
+const path = require('path');
 const app = express();
+const request = require('request');
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views') )
+app.set('views engine', 'ejs', );
+
+
+request('https://routerover3.onrender.com/ver', function (error, response , body){
+    console.error(error);
+    console.log('statusCode', response && response.statusCode);
+    console.log('body:', body);
+});
+
+app.listen(app.get('port'), ()=>{
+    console.log('Servidor escuchando em el puerto:', app.get('port'));
+});
