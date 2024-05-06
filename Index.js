4ae37efed74024a56971cb94da37adc041e85a3b
kevinmedrano21 <kmedrano433@gmail.com>
2024-05-05 23:29:07 -0600

    ejs añadido

--- Index.js
+++ Index.js
@@ -7,12 +7,27 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views') )
 app.set('views engine', 'ejs', );
 
+const indexRoutes = require('./Routes/index.js');
+app.use('/',indexRoutes);
+
 
 request('https://routerover3.onrender.com/ver', function (error, response , body){
     console.error(error);
     console.log('statusCode', response && response.statusCode);
     console.log('body:', body);
 });
+
+// fetch('https://routerover3.onrender.com/ver')
+//     .then(response => response.json)
+//     .then(data => {
+//         console.log(data)
+//     })
+
+
+
+//adminlte
+// app.use('/admin', express.static('./node_modules/admin-lte-express/public'))
+// app.use('/', require('admin-lte-express'))
 
 app.listen(app.get('port'), ()=>{
     console.log('Servidor escuchando em el puerto:', app.get('port'));
